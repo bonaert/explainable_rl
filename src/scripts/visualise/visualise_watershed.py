4a9eeea55815f6fc5cf82b390d371a6c3d0923be
bonaert <harokb@gmail.com>
2020-02-23 17:13:20 +0100

    Possible bug: Discovered that testing the policy gives different average results in different runs

--- src/scripts/visualise/visualise_watershed.py
+++ src/scripts/visualise/visualise_watershed.py
@@ -4,5 +4,10 @@ from training.sac import sac_run_from_disk
 
 if __name__ == "__main__":
     env = gym.make('watershed-v0', limited_scenarios=False, bizarre_actions=True)
-    sac_run_from_disk(env, has_scaler=False, render=True, run_once=True)
+
+    # There might be a bug, because the average last reward is sometimes 185 +- 25
+    # and other times it's 205 +- 25. This isn't normal at all.
+    # TODO: find out why this strange behavior happens and if it's due to an error / bug, fix it!
+
+    sac_run_from_disk(env, has_scaler=False, render=False, run_once=False)
     env.close()  # To avoid benign but annoying errors when the gym render window closes
