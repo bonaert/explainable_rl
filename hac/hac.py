b64ad055831ca89cecb799552b4cd0c0b81fce51
bonaert <harokb@gmail.com>
2020-04-08 21:53:32 +0200

    Improve code organisation so that it's much easier to do experiments

--- hac/hac.py
+++ hac/hac.py
@@ -16,6 +16,11 @@ from torch.optim import Adam
 from tqdm import tqdm
 
 
+ALWAYS = 2
+FIRST_RUN = 1
+NEVER = 0
+
+
 def get_tensor(x: Union[np.ndarray, torch.Tensor]) -> torch.Tensor:
     if torch.is_tensor(x):
         return x
@@ -460,11 +465,12 @@ def run_hac(hac_params: HacParams, start_state: np.ndarray, goal_state: np.ndarr
 
 
 def evaluate_hac(hac_params: HacParams, env: gym.Env, goal_state: np.ndarray,
-                 num_evals: int = 5, always_render: bool = False) -> Tuple[int, float]:
+                 render_frequency: int, num_evals: int = 5) -> Tuple[int, float]:
     num_successes = 0
     for i in range(num_evals):
         state = env.reset()
-        _, failed = run_hac(hac_params, state, goal_state, env, training=False, render=(i == 0 or always_render))
+        render_now = (render_frequency == ALWAYS) or (render_frequency == FIRST_RUN and i == 0)
+        _, failed = run_hac(hac_params, state, goal_state, env, training=False, render=render_now)
 
         if not failed:
             num_successes += 1
@@ -473,18 +479,19 @@ def evaluate_hac(hac_params: HacParams, env: gym.Env, goal_state: np.ndarray,
     return num_successes, success_rate
 
 
-def train(hac_params: HacParams, env: gym.Env, goal_state: np.ndarray, directory: str):
+def train(hac_params: HacParams, env: gym.Env, goal_state: np.ndarray, render_frequency: int, directory: str):
     for i in tqdm(range(hac_params.num_training_episodes)):
-        # Train
+        # Train HAC
         state = env.reset()
         run_hac(hac_params, state, goal_state, env, training=True, render=False)
         update_networks(hac_params)
 
-        # Test
+        # Evaluate HAC
         if i == 0 or (i + 1) % hac_params.evaluation_frequency == 0:
-            num_successes, success_rate = evaluate_hac(hac_params, env, goal_state)
+            num_successes, success_rate = evaluate_hac(hac_params, env, goal_state, render_frequency)
             print("\nStep %d: Success rate (%d/5): %.3f" % (i + 1, num_successes, success_rate))
 
+        # Save HAC policies and params
         if (i + 1) % hac_params.save_frequency == 0:
             save_hac(hac_params, directory)
 
@@ -524,7 +531,7 @@ def load_hac(directory: str = ".") -> HacParams:
         # Loop over a view to allow modification of the original dict as we iterate over it
         for key, value in hac_params_as_dict.items():
             if type(value) == list and key not in ["policies", "her_storage"]:
-                hac_params_as_dict[key] = np.array(value)
+                hac_params_as_dict[key] = np.array(value, dtype=np.float32)
 
         hac_params = HacParams(**hac_params_as_dict)
 
@@ -539,77 +546,109 @@ def load_hac(directory: str = ".") -> HacParams:
 
 
 if __name__ == '__main__':
-    # current_env = gym.make("MountainCarContinuous-v0")
-    # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L45
-    # goal_state = np.array([0.48, 0.04])
+    # noinspection PyUnreachableCode
+    ##################################
+    #     Environment parameters     #
+    ##################################
 
-    current_env = gym.make("Pendulum-v0")
-    current_goal_state = np.array([0.0, 1.0, 0.0])
+    # env_name = "AntMaze"
+    env_name = "MountainCar"
+    # env_name = "AntMaze"
+    if env_name == "AntMaze":
+        # distance_thresholds = [0.1, 0.1]  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L106
+        # max_horizons = 10  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L27
+        # action_noise_coeffs = np.array([0.1] * current_action_size),  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L131
+        # subgoal_noise_coeffs = np.array([0.03] * current_state_size),  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L132
+        raise Exception("TODO")
+    elif env_name == "MountainCar":
+        current_env = gym.make("MountainCarContinuous-v0")
+        current_goal_state = np.array([0.48, 0.04])  # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L45
 
-    # For MountainCar, the values for the HacParams were taken from here:
-    # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py
+        # noinspection PyUnreachableCode
+        if False:
+            num_levels = 2
+            max_horizons = [20, 20]  # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L50
+            # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L46
+            distance_thresholds = [[0.01, 0.02],    # We want to have precise subgoals
+                                   [0.05, 10.0]]   # But for the goal I only care about the position (not the speed)
+        else:
+            num_levels = 3
+            max_horizons = [10, 10, 10]
+            # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L46
+            distance_thresholds = [[0.01, 0.02],   # We want to have precise subgoals
+                                   [0.01, 0.02],
+                                   [0.05, 10.0]]    # But for the goal I only care about the position (not the speed)
+
+        action_noise_coeffs=np.array([0.1])  # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L42
+        subgoal_noise_coeffs = np.array([0.02, 0.01])  # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L43
+    elif True:
+        current_env = gym.make("Pendulum-v0")
+        current_goal_state = np.array([0.0, 1.0, 0.0])
+
+        num_levels = 2
+        max_horizons = [15, 15]
+        distance_thresholds = [[0.10, 0.10, 1.0],  # Pendulum state = (x, y, angular velocity)
+                               [0.05, 0.05, 0.2]]
+
+        action_noise_coeffs = np.array([0.05])
+        subgoal_noise_coeffs = np.array([0.02, 0.02, 0.3])
 
     current_state_size = current_env.observation_space.low.shape[0]
     current_action_size = current_env.action_space.low.shape[0]
+
+    ########################################
+    #     Regularly changed parameters     #
+    ########################################
+    current_directory = f"{env_name}_{num_levels}_levels_h_{'_'.join(map(str, max_horizons))}"
+    currently_training = True
+    render_frequency = FIRST_RUN
+    num_training_episodes = 500
+
+    #############################
+    #     Shared parameters     #
+    #############################
+
+    # batch_size=1024  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/layer.py#L43
+    batch_size = 128  # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L56
+
+    # discount=0.98  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/master/critic.py#L8
+    discount = 0.95
+
+    # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L54
+    # Note: this parameters is actually more complicated than this, because the buffer size depends on the level
+    # but currently, we're simplying it to a simple constant. TODO: see if this needs fixing
+    # replay_buffer_size=10**7,  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/layer.py#L25
+    replay_buffer_size = 500_000  # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/117d4002e754a53019b5cf7f103946d382488217/utils.py#L4
+    subgoal_testing_frequency = 0.3  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L125
+    num_update_steps_when_training = 40  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/agent.py#L40
+    evaluation_frequency = 50  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L138
 
     current_hac_params = HacParams(
         action_low=current_env.action_space.low,
         action_high=current_env.action_space.high,
         state_low=current_env.observation_space.low,
         state_high=current_env.observation_space.high,
-
-        # batch_size=1024,  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/layer.py#L43
-        batch_size=100,
-        # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L56
-        num_training_episodes=700,
-        # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L138
-        num_levels=3,
-        # max_horizons=[10, 10],  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L27
-        max_horizons=[16, 8, 8],  # Lower levels have the right to more steps. We want few high level goals.
-        # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L50
-        # discount=0.98,  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/master/critic.py#L8
-        discount=0.95,
-        # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L54
-        # Note: this parameters is actually more complicated than this, because the buffer size depends on the level
-        # but currently, we're simplying it to a simple constant. TODO: see if this needs fixing
-        # replay_buffer_size=10**7,  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/layer.py#L25
-        replay_buffer_size=5 * 10 ** 5,
-        # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/117d4002e754a53019b5cf7f103946d382488217/utils.py#L4
-        subgoal_testing_frequency=0.3,
-        # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L125
-        # They have threshold for Mujoco agents, but here it's MountainCar, so I'll have to figure out for myself what good distance threshold are
-        # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L106
-        # distance_thresholds=[0.1, 0.1],
-        # distance_thresholds=[[0.01, 0.02],  # We want to have precise subgoals
-        #                      [0.01, 0.02],
-        #                      [0.05, 10]],    # But for the goal I only care about the position (not the speed)
-        distance_thresholds=[[0.02, 0.02, 0.1],  # Pendulum state = (x, y, angular velocity)
-                             [0.02, 0.02, 0.1],
-                             [0.02, 0.02, 0.05]],
-        # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L46
-        # action_noise_coeffs=np.array([0.1] * current_action_size),  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L131
-        # subgoal_noise_coeffs=np.array([0.03] * current_state_size),  # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/design_agent_and_env.py#L132
-        # action_noise_coeffs=np.array([0.1]),  # MC
-        action_noise_coeffs=np.array([0.1]),
-        # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L42
-        # subgoal_noise_coeffs=np.array([0.02, 0.01]), # MC
-        subgoal_noise_coeffs=np.array([0.02, 0.02, 0.02]),
-        # https://github.com/nikhilbarhate99/Hierarchical-Actor-Critic-HAC-PyTorch/blob/master/train.py#L43
-
-        num_update_steps_when_training=40,
-        # https://github.com/andrew-j-levy/Hierarchical-Actor-Critc-HAC-/blob/f90f2c356ab0a95a57003c4d70a0108f09b6e6b9/agent.py#L40
-
-        evaluation_frequency=50,
-
-        save_frequency=50
+        batch_size=batch_size,
+        num_training_episodes=num_training_episodes,
+        num_levels=num_levels,
+        max_horizons=max_horizons,
+        discount=discount,
+        replay_buffer_size=replay_buffer_size,
+        subgoal_testing_frequency=subgoal_testing_frequency,
+        distance_thresholds=distance_thresholds,
+        action_noise_coeffs=action_noise_coeffs,
+        subgoal_noise_coeffs=subgoal_noise_coeffs,
+        num_update_steps_when_training=num_update_steps_when_training,
+        evaluation_frequency=evaluation_frequency,
+        save_frequency=evaluation_frequency
     )
 
-    # current_directory = "mountain_car_3_levels_h_4_8_16"
-    current_directory = "pendulum_3_levels_h_4_8_16"
-    currently_training = True
+    print("Action space: Low %s\tHigh %s" % (current_env.action_space.low, current_env.action_space.high))
+    print("State space: Low %s\tHigh %s" % (current_env.observation_space.low, current_env.observation_space.high))
+
     if currently_training:
-        train(current_hac_params, current_env, current_goal_state, directory=current_directory)
+        train(current_hac_params, current_env, current_goal_state, render_frequency, directory=current_directory)
     else:
         current_hac_params = load_hac(current_directory)
-        evaluate_hac(current_hac_params, current_env, current_goal_state, num_evals=100, always_render=True)
+        evaluate_hac(current_hac_params, current_env, current_goal_state, render_frequency, num_evals=100)
 
